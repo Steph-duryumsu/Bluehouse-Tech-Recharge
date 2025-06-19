3744168b9ba2123452d313f60606825b50a032ae
Gisrobin <gisrobinezeobi@gmail.com>
2025-06-19 14:34:18 -0700

    adjusted body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // src/App.js
 import React from 'react';
-
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import SignInPage from './Pages/SignInPage';
@@ -11,10 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/welcome" replace />} />
-        <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/signin" element={<SignInPage />} />
+        {/* <Route path="/" element={<Navigate to="/welcome" replace />} /> */}
+        <Route path="/" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
         {/* add more routes here as needed */}
       </Routes>
     </BrowserRouter>
